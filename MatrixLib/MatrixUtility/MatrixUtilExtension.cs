6aedf100e3790ecf18b2d4c65e1d18bcef447da3
superfooman <bugaboo8834@hotmail.com>
2018-04-19 01:01:16 -0700

    Create extension utility class template

---
+++ MatrixLib/MatrixUtility/MatrixUtilExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLib.MatrixUtility
+{
+    // i.e. 
+    // Matrix<int> matrix = new Matrix<int>();
+    // int number = matrix.getCalcResult();
+
+    public class Matrix<T> where T : new()
+    {
+        public T[,] values;
+    }
+
+    public static class MatrixExtension
+    {
+        public static T getCalcResult<T>(this Matrix<T> matrix) where T : new()
+        {
+            T result = new T();
+            // TO-DO
+            return result;
+        }
+    }
+
+}
